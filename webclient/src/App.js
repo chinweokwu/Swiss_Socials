96a5bf6eef5b127989db827bbf950d458b720023
chinweokwu <paulcmorah@gmail.com>
2023-01-30 10:10:16 +0100

    add navbar

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -1,9 +1,69 @@
+import React from 'react';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
+import Home from './Pages/Home/Home';
+import NavBar from './Components/NavBar/NavBar';
+import RightBar from './Components/RightBar/RightBar';
+import LeftBar from './Components/LeftBar/LeftBar';
+import Profile from './Pages/Profile/Profile';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+  Outlet
+} from "react-router-dom";
+
 
 function App() {
+  const currentUser = true;
+  const Layout = () =>{
+    return (
+      <div>
+        <NavBar/>
+        <div style={{display: "flex"}}>
+          <LeftBar/>
+          <Outlet/>
+          <RightBar/>
+        </div>
+      </div>
+    )
+  }
+
+  const ProtectedRoute = ({children}) => {
+    if(!currentUser){
+      return <Navigate to="/login"/>
+    }
+    return children
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (<ProtectedRoute><Layout /></ProtectedRoute>),
+      children: [
+        {
+          path: "/",
+          element: <Home/>,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile/>,
+        },
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+    {
+      path: "/register",
+      element: <Register/>,
+    }
+  ]);
+
   return (
     <div className="App">
-
-      Swiss Socials
+      <RouterProvider router={router}/>
     </div>
   );
 }
--- webclient/src/Pages/Login/Login.js
+++ webclient/src/Pages/Login/Login.js
@@ -1,29 +1,59 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import './login.scss';
 
 const Login = () => {
   return (
-    <div className='login'>
-      <div className='auth_card'>
-        <div className='auth-left'>
-          <h1>Swiss Socials.</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Voluptatibus, quod. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Voluptatibus, quod. Lorem ipsum dolor sit
-          </p>
-          <span>Don't you have an account?</span>
-          <button>Register</button>
-        </div>
-        <div className='auth-right'>
-            <h1>Login</h1>
-            <form>
-              <input type='text' placeholder='username'/>
-              <input type='password' placeholder='password'/>
-              <button>Login</button>
-            </form>
+    <div className="bg-white dark:bg-gray-900">
+        <div className="flex justify-center h-screen">
+            <div className="hidden bg-cover lg:block lg:w-2/3">
+                <div className="flex items-center h-full px-20 bg-gray-900 bg-opacity-40 bg-img">
+                    <div>
+                        <h2 className="text-4xl font-bold text-white">Swiss Socials</h2>
+                        
+                        <p className="max-w-xl mt-3 text-gray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit. In autem ipsa, nulla laboriosam dolores, repellendus perferendis libero suscipit nam temporibus molestiae</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
+                <div className="flex-1">
+                    <div className="text-center">
+                        <h2 className="text-4xl font-bold text-center text-gray-700 dark:text-white">Brand</h2>
+                        
+                        <p className="mt-3 text-gray-500 dark:text-gray-300">Welcome Back</p>
+                    </div>
 
+                    <div className="mt-8">
+                        <form>
+                            <div>
+                                <label  htmlFor="email" className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email Address</label>
+                                <input type="email" name="email" id="email" placeholder="example@example.com" className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                            </div>
+
+                            <div className="mt-6">
+                                <div className="flex justify-between mb-2">
+                                    <label  htmlFor="password" className="text-sm text-gray-600 dark:text-gray-200">Password</label>
+                                    <Link to="#" className="text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline">Forgot password?</Link>
+                                </div>
+
+                                <input type="password" name="password" id="password" placeholder="Your Password" className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                            </div>
+
+                            <div className="mt-6">
+                                <button
+                                    className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                                    Sign in
+                                </button>
+                            </div>
+
+                        </form>
+
+                        <p className="mt-6 text-sm text-center text-gray-400">Don&#x27;t have an account yet? <Link to="/register" className="text-blue-500 focus:outline-none focus:underline hover:underline">Sign up</Link>.</p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
   )
 }
