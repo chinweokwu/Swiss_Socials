e7e45a88e9fac17127291e10a6bfc062449a8463
chinweokwu <paulcmorah@gmail.com>
2023-02-04 15:37:24 +0100

    fix failing code

--- webclient/src/Components/Comments/Comments.js
+++ webclient/src/Components/Comments/Comments.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./comments.scss";
-
+import { Link } from 'react-router-dom';
 const Comments = () => {
   const comments = [
     {
@@ -20,49 +20,49 @@ const Comments = () => {
   ]
   return (
     <div className='comments'>
-      <div class="relative">
+      <div className="relative">
         <input
-          class="pt-2 pb-2 pl-3 w-full h-11 bg-slate-100 dark:bg-slate-600 rounded-lg placeholder:text-slate-600 dark:placeholder:text-slate-300 font-medium pr-20"
+          className="pt-2 pb-2 pl-3 w-full h-11 bg-slate-100 dark:bg-slate-600 rounded-lg placeholder:text-slate-600 dark:placeholder:text-slate-300 font-medium pr-20"
           type="text" placeholder="Write a comment" />
-        <span class="flex absolute right-3 top-2/4 -mt-3 items-center">
-          <svg class="mr-2" style={{width: "24px", height: "24px"}} viewBox="0 0 24 24">
+        <span className="flex absolute right-3 top-2/4 -mt-3 items-center">
+          <svg className="mr-2" style={{width: "24px", height: "24px"}} viewBox="0 0 24 24">
             <path fill="currentColor"
               d="M20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M10,9.5C10,10.3 9.3,11 8.5,11C7.7,11 7,10.3 7,9.5C7,8.7 7.7,8 8.5,8C9.3,8 10,8.7 10,9.5M17,9.5C17,10.3 16.3,11 15.5,11C14.7,11 14,10.3 14,9.5C14,8.7 14.7,8 15.5,8C16.3,8 17,8.7 17,9.5M12,17.23C10.25,17.23 8.71,16.5 7.81,15.42L9.23,14C9.68,14.72 10.75,15.23 12,15.23C13.25,15.23 14.32,14.72 14.77,14L16.19,15.42C15.29,16.5 13.75,17.23 12,17.23Z">
             </path>
           </svg>
-          <svg class="fill-blue-500 dark:fill-slate-50"style={{width: "24px", height: "24px"}} viewBox="0 0 24 24">
+          <svg className="fill-blue-500 dark:fill-slate-50"style={{width: "24px", height: "24px"}} viewBox="0 0 24 24">
             <path d="M2,21L23,12L2,3V10L17,12L2,14V21Z"></path>
           </svg>
         </span>
       </div>
-      <div class="pt-6">
+      <div className="pt-6">
         {
           comments.map(comment => (
-            <div class="media flex pb-4">
-            <a class="mr-4" href="#">
-              <img class="rounded-full max-w-none w-12 h-12" src={comment.profilePic} />
-            </a>
-            <div class="media-body">
+            <div className="media flex pb-4">
+            <Link className="mr-4" to="#">
+              <img className="rounded-full max-w-none w-12 h-12" src={comment.profilePic} alt='' />
+            </Link>
+            <div className="media-body">
               <div>
-                <a class="inline-block text-base font-bold mr-2" href="#">{comment.name}</a>
-                <span class="text-slate-500 dark:text-slate-300">25 minutes ago</span>
+                <Link className="inline-block text-base font-bold mr-2" to="#">{comment.name}</Link>
+                <span className="text-slate-500 dark:text-slate-300">25 minutes ago</span>
               </div>
               <p>
                 {comment.desc}
               </p>
-              <div class="mt-2 flex items-center">
-                <a class="inline-flex items-center py-2 mr-3" href="#">
-                  <span class="mr-2">
-                    <svg class="fill-rose-600 dark:fill-rose-400" style={{width: "24px", height: "24px"}}
+              <div className="mt-2 flex items-center">
+                <Link className="inline-flex items-center py-2 mr-3" to="#">
+                  <span className="mr-2">
+                    <svg className="fill-rose-600 dark:fill-rose-400" style={{width: "24px", height: "24px"}}
                       viewBox="0 0 24 24">
                       <path
                         d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z">
                       </path>
                     </svg>
                   </span>
-                  <span class="text-base font-bold">2</span>
-                </a>
-                <button class="py-2 px-4 font-medium hover:bg-slate-50 dark:hover:bg-slate-700 rounded-lg">
+                  <span className="text-base font-bold">2</span>
+                </Link>
+                <button className="py-2 px-4 font-medium hover:bg-slate-50 dark:hover:bg-slate-700 rounded-lg">
                   Repply
                 </button>
               </div>
@@ -71,9 +71,9 @@ const Comments = () => {
           ))
         }
           <div class="w-full">
-            <a href="#"
+            <Link to="#"
               class="btn-comments py-3 px-4 w-full block text-center rounded-lg font-medium hover:bg-slate-200 dark:hover:bg-slate-600 transition ease-in-out delay-75">Show
-              more comments</a>
+              more comments</Link>
           </div>
         </div>
     </div>
--- webclient/src/Components/RightBar/RightBar.js
+++ webclient/src/Components/RightBar/RightBar.js
@@ -4,129 +4,129 @@ const RightBar = () => {
   return (
     <div className='rightbar'>
       <div className='container'>
-        <card class="card rounded-lg flex flex-col p-5 gap-5">
-          <header class="font-semibold text-xl">
+        <div className="card rounded-lg flex flex-col p-5 gap-5">
+          <header className="font-semibold text-xl">
             Suggestions for you
           </header>
-          <content class="grid grid-cols-6">
+          <div className="grid grid-cols-6">
             <div>
-              <img src="https://picsum.photos/70/70" class="rounded-full" alt=''/>
+              <img src="https://picsum.photos/70/70" className="rounded-full" alt=''/>
             </div>
-            <div class="col-span-5 flex flex-col gap-1 pl-3">
-              <name class="font-semibold">Flip</name>
-              <description class="text-gray-500 text-sm">Company . Financial Services</description>
-              <div class="mt-1">
-                <button class="hover:border-gray-900 hover:bg-gray-300 delay-100 duration-100 border border-gray-500 rounded-full text-gray-700 inline-flex px-3 py-1 text-md">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <div className="col-span-5 flex flex-col gap-1 pl-3">
+              <p className="font-semibold">Flip</p>
+              <p className="text-gray-500 text-sm">Company . Financial Services</p>
+              <div className="mt-1">
+                <button className="hover:border-gray-900 hover:bg-gray-300 delay-100 duration-100 border border-gray-500 rounded-full text-gray-700 inline-flex px-3 py-1 text-md">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
                   </svg>
                     Follow
                 </button>
-                <button class=" mx-3 hover:border-gray-900 hover:bg-gray-300 delay-100 duration-100 border border-gray-500 rounded-full text-gray-700 inline-flex px-3 py-1 text-md">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <button className=" mx-3 hover:border-gray-900 hover:bg-gray-300 delay-100 duration-100 border border-gray-500 rounded-full text-gray-700 inline-flex px-3 py-1 text-md">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
                   </svg>
                     Dismiss
                 </button>
               </div>
             </div>
-          </content>
-          <content class="grid grid-cols-6">
+          </div>
+          <div className="grid grid-cols-6">
             <div>
-              <img src="https://picsum.photos/70/70" class="rounded-full" alt=''/>
+              <img src="https://picsum.photos/70/70" className="rounded-full" alt=''/>
             </div>
-            <div class="col-span-5 flex flex-col gap-1 pl-3">
-              <name class="font-semibold">Flip</name>
-              <description class="text-gray-500 text-sm">Company . Financial Services</description>
-              <div class="mt-1">
-                <button class=" hover:border-gray-900 hover:bg-gray-300 delay-100 duration-100 border border-gray-500 rounded-full text-gray-700 inline-flex px-3 py-1 text-md">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <div className="col-span-5 flex flex-col gap-1 pl-3">
+              <p className="font-semibold">Flip</p>
+              <p className="text-gray-500 text-sm">Company . Financial Services</p>
+              <div className="mt-1">
+                <button className=" hover:border-gray-900 hover:bg-gray-300 delay-100 duration-100 border border-gray-500 rounded-full text-gray-700 inline-flex px-3 py-1 text-md">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
                   </svg>
                     Follow
                 </button>
-                <button class="mx-3 hover:border-gray-900 hover:bg-gray-300 delay-100 duration-100 border border-gray-500 rounded-full text-gray-700 inline-flex px-3 py-1 text-md">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <button className="lg:mx-3 hover:border-gray-900 hover:bg-gray-300 delay-100 duration-100 border border-gray-500 rounded-full text-gray-700 inline-flex px-3 py-1 text-md">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
                   </svg>
                     Dismiss
                 </button>
               </div>
             </div>
-          </content>
-          <footer class="text-gray-500">
-            <button class="hover:bg-gray-200 delay-100 duration-100 inline-flex items-center px-3 py-1 rounded">
+          </div>
+          <footer className="text-gray-500">
+            <button className="hover:bg-gray-200 delay-100 duration-100 inline-flex items-center px-3 py-1 rounded">
                   View all recommendations
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
               </svg>
             </button>
           </footer>
-        </card>
+        </div>
 
-        <card class="rounded-lg flex flex-col p-5 gap-5 my-5">
-          <header class="font-semibold text-xl">
+        <div className=" card rounded-lg flex flex-col p-5 gap-5 my-5">
+          <header className="font-semibold text-xl">
             Latest Activites
           </header>
-          <content class="grid grid-cols-6">
+          <div className="grid grid-cols-6">
             <div>
-              <img src="https://picsum.photos/70/70" class="rounded-full" alt=''/>
+              <img src="https://picsum.photos/70/70" className="rounded-full" alt=''/>
             </div>
-            <div class="col-span-5 flex flex-col gap-1 pl-3">
-              <name class="font-semibold">Flip</name>
-              <description class="text-gray-500 text-sm">comment on a post</description>
-              <date class="text-gray-500 text-sm">1 mins</date>
+            <div className="col-span-5 flex flex-col gap-1 pl-3">
+              <p className="font-semibold">Flip</p>
+              <p className="text-gray-500 text-sm">comment on a post</p>
+              <p className="text-gray-500 text-sm">1 mins</p>
             </div>
-          </content>
-          <content class="grid grid-cols-6">
+          </div>
+          <div className="grid grid-cols-6">
             <div>
-              <img src="https://picsum.photos/70/70" class="rounded-full" alt=''/>
+              <img src="https://picsum.photos/70/70" className="rounded-full" alt=''/>
             </div>
-            <div class="col-span-5 flex flex-col gap-1 pl-3">
-              <name class="font-semibold">Flip</name>
-              <description class="text-gray-500 text-sm">Liked a post</description>
-              <date class="text-gray-500 text-sm">1 mins</date>
+            <div className="col-span-5 flex flex-col gap-1 pl-3">
+              <p className="font-semibold">Flip</p>
+              <p className="text-gray-500 text-sm">Liked a post</p>
+              <p className="text-gray-500 text-sm">1 mins</p>
             </div>
-          </content>
-          <footer class="text-gray-500">
-            <button class="hover:bg-gray-200 delay-100 duration-100 inline-flex items-center px-3 py-1 rounded">
+          </div>
+          <footer className="text-gray-500">
+            <button className="hover:bg-gray-200 delay-100 duration-100 inline-flex items-center px-3 py-1 rounded">
                   View all latest activites
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
               </svg>
             </button>
           </footer>
-        </card>
+        </div>
 
-        <card class="rounded-lg flex flex-col p-5 gap-5 my-5">
-          <header class="font-semibold text-xl">
+        <div className="card rounded-lg flex flex-col p-5 gap-5 my-5">
+          <header className="font-semibold text-xl">
             Online Friends
           </header>
-          <content class="grid grid-cols-6">
+          <div className="grid grid-cols-6">
             <div>
-              <img src="https://picsum.photos/70/70" class="rounded-full" alt=''/>
+              <img src="https://picsum.photos/70/70" className="rounded-full" alt=''/>
             </div>
-            <div class="mt-3 col-span-5 flex flex-col gap-1 pl-3">
-              <name class="font-semibold">Flip</name>
+            <div className="mt-3 col-span-5 flex flex-col gap-1 pl-3">
+              <p className="font-semibold">Flip</p>
             </div>
-          </content>
-          <content class="grid grid-cols-6">
+          </div>
+          <div className="grid grid-cols-6">
             <div>
-              <img src="https://picsum.photos/70/70" class="rounded-full" alt=''/>
+              <img src="https://picsum.photos/70/70" className="rounded-full" alt=''/>
             </div>
-            <div class=" mt-3 col-span-5 flex flex-col gap-1 pl-3">
-              <name class="font-semibold">Flip</name>
+            <div className=" mt-3 col-span-5 flex flex-col gap-1 pl-3">
+              <p className="font-semibold">Flip</p>
             </div>
-          </content>
-          <footer class="text-gray-500">
-            <button class="hover:bg-gray-200 delay-100 duration-100 inline-flex items-center px-3 py-1 rounded">
+          </div>
+          <footer className="text-gray-500">
+            <button className="hover:bg-gray-200 delay-100 duration-100 inline-flex items-center px-3 py-1 rounded">
                   View all onlinefriends
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
               </svg>
             </button>
           </footer>
-        </card>
+        </div>
       </div>
     </div>
   )
